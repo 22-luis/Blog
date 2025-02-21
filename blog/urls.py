e06a6a22583b223bdeb9907ce1d8168af38dd4ee
22-luis <00016720@uca.edu.sv>
2025-02-21 11:15:53 -0600

    update: crud

--- blog/urls.py
+++ blog/urls.py
@@ -5,4 +5,7 @@ urlpatterns = [
     path('', views.home, name='home'),  # Página principal
     path('post/<int:post_id>/', views.post_detail, name='post_detail'),  # Detalle de un post
     path('new/', views.new_post, name='new_post'),  # Crear un post
+    path('delete/<int:post_id>', views.delete_post, name='delete_post'), #eliminar un post
+    path('edit/<int:post_id>', views.update_post, name='update_post'), #editar un post
+    
 ]
--- blog/views.py
+++ blog/views.py
@@ -16,3 +16,17 @@ def new_post(request):
         Post.objects.create(title=title, content=content)
         return redirect('home')
     return render(request, 'blog/new_post.html')
+
+def delete_post(request, post_id):
+    if request.method == 'POST':
+        post = get_object_or_404(Post, id=post_id)
+        post.delete()
+        return redirect('home')
+        
+
+def update_post(request, post_id):
+    if request.method == 'POST':
+        post = get_object_or_404(Post, id=post_id)
+        post.update()
+        return redirect('home')
+    return render(request, 'blog/update_post.html')
